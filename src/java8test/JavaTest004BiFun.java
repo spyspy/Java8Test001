f6e8e81550b7f5add5deebeafbb6e7bc2651a853
spyspy <spyhopping101@gmail.com>
2019-05-02 14:43:14 +0800

    GO2

--- src/java8test/JavaTest004BiFun.java
+++ src/java8test/JavaTest004BiFun.java
@@ -2,6 +2,7 @@ package java8test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 public class JavaTest004BiFun {
@@ -15,18 +16,27 @@ public class JavaTest004BiFun {
         List<Person> psResult = test.getPersonByName("Apple", ps);
         psResult.forEach(p->System.out.println(p.getName()+":"+p.getAge()));
 
+        System.out.println("=========================================");
+
+        List<Person> psResult2 = test.getPersonByAge(25, ps);
+        psResult2.forEach(p->System.out.println(p.getName()+":"+p.getAge()));
+
+        System.out.println("=========================================");
+
     }
 
     //find some conditions in List<Person>
     //use functional way....only one line?
-    public List<Person> getPersonByName(String name,List<Person>ps){
+    private List<Person> getPersonByName(String name,List<Person>ps){
         return ps.stream().filter(person -> person.getName().equals(name)).collect(Collectors.toList());
     }
 
     //search by age
-    //Analysis:
-    public List<Person> getPersonByAge(int age,List<Person>ps){
-        return ps.stream().filter(person -> person.getAge() == (age)).collect(Collectors.toList());
+    //Analysis: Two parameget and One Return... JUST like BiFunction
+    private List<Person> getPersonByAge(int age,List<Person>ps){
+        //return ps.stream().filter(person -> person.getAge() == (age)).collect(Collectors.toList());
+        BiFunction<Integer,List<Person>,List<Person>> biFun = (biAge,biPs)->biPs.stream().filter(per ->per.getAge()>biAge).collect(Collectors.toList());
+        return biFun.apply(age,ps);
     }
 }
 
