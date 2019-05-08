44ed9f30201859698d47b29619c49de716c7cd37
spyspy <spyhopping101@gmail.com>
2019-05-08 14:11:18 +0800

    20190508-02

--- src/java8test/Java8Test007MethodRef.java
+++ src/java8test/Java8Test007MethodRef.java
@@ -15,6 +15,62 @@ public class Java8Test007MethodRef {
         listString.forEach(System.out::println);
         System.out.println("***********************************");
 
+        //There are 4 types of Method References
+        //Type 1.  class name :: static method name
 
+
+        //Type 1 Example:
+        MyStudent stu1 = new MyStudent("S1",15);
+        MyStudent stu2 = new MyStudent("S2",95);
+        MyStudent stu3 = new MyStudent("S3",55);
+        MyStudent stu4 = new MyStudent("S4",45);
+        List<MyStudent> students = Arrays.asList(stu1,stu2,stu3,stu4);
+
+        //Sort method will change Object itself
+        //Lambda way
+        //students.sort((ss1,ss2)->MyStudent.compareStudentByScore(ss1,ss2));
+        //students.forEach(ss1-> System.out.println(ss1.getScore()));
+        System.out.println("**************************");
+        //Method Reference way
+        students.sort(MyStudent::compareStudentByScore);
+
+
+
+    }
+}
+
+class MyStudent{
+    private String name;
+
+    private int score;
+
+    //static method: order by name and score;
+    public static int compareStudentByScore(MyStudent s1,MyStudent s2){
+        return s1.getScore()-s2.getScore();
+    }
+
+    public static int compareStudentByName(MyStudent s1,MyStudent s2){
+        return s1.getName().compareToIgnoreCase(s2.getName());
+    }
+
+    public MyStudent(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 }
