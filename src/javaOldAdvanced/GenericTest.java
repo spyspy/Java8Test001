3cacb8c5212a81373cafd423df89689f1ef6c968
spyspy <spyhopping101@gmail.com>
2019-06-04 17:26:46 +0800

    20190604-01

--- src/javaOldAdvanced/GenericTest.java
+++ src/javaOldAdvanced/GenericTest.java
@@ -1,7 +1,15 @@
 package javaOldAdvanced;
 
+import java.util.*;
+
 public class GenericTest {
     public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("WOW","TEST");
+
+        List<String> list2 = new ArrayList<>();
+
+        List<? extends List<String>> list3 = new ArrayList<>();
 
     }
 }
@@ -24,5 +32,21 @@ class GenericObject1<T,K>{
 
     public void setObj2(K obj2) {
         this.obj2 = obj2;
+    }
+}
+
+class GenericTest2<T>{
+    private T obj1;
+
+    public T getObj1() {
+        return obj1;
+    }
+
+    public void setObj1(T obj1) {
+        this.obj1 = obj1;
+    }
+
+    public void test01(List<? extends List> list){
+
     }
 }
