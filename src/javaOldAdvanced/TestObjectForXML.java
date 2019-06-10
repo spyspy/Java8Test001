557b76fdaff9f63878eba62dbe260794f700b5de
Nelson Lin <spyhopping101@gmail.cm>
2019-06-10 22:27:03 +0800

    20190610-01

---
+++ src/javaOldAdvanced/TestObjectForXML.java
@@ -0,0 +1,39 @@
+package javaOldAdvanced;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class TestObjectForXML {
+    private String test111;
+    private String test222;
+    private String test333;
+
+    public String getTest111() {
+        return test111;
+    }
+
+    public void setTest111(String test111) {
+        this.test111 = test111;
+    }
+
+    public String getTest222() {
+        return test222;
+    }
+
+    public void setTest222(String test222) {
+        this.test222 = test222;
+    }
+
+    public String getTest333() {
+        return test333;
+    }
+
+    public void setTest333(String test333) {
+        this.test333 = test333;
+    }
+}
