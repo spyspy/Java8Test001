8859f03857f94572bbe63bb062050d4ec8286dd7
Nelson <spyhopping101@gmail.com>
2020-06-24 08:49:37 +0800

    2020/06/24 update

--- src/javaAdvanced/base64Test/FromObjToJsonToBase64ToMd5.java
+++ src/javaAdvanced/base64Test/FromObjToJsonToBase64ToMd5.java
@@ -18,12 +18,12 @@ public class FromObjToJsonToBase64ToMd5 {
 
         //To Json
         Gson gson = new Gson();
-        String gsonstr = gson.toJson(tc).toString();
+        String gsonstr = gson.toJson(tc);
         System.out.println(gsonstr);
 
-        //To base64
+        //To base64 String
         String base64str =Base64.getEncoder().encodeToString(gsonstr.getBytes());
-        System.out.println(base64str);
+        System.out.println("base64str:"+base64str);
 
         //To MD5
         MessageDigest mg =MessageDigest.getInstance("MD5");
@@ -31,15 +31,30 @@ public class FromObjToJsonToBase64ToMd5 {
         mg.toString();
         System.out.println(mg.toString());
 
+        //To base64 byte[]
+        byte[] base64byte =Base64.getEncoder().encode(gsonstr.getBytes());
+        String base64byteToStr = new String(base64byte); //Notice Point!!!!
 
 
+        //MD5 Test2
         String password = "ILoveJava";
         MessageDigest md = MessageDigest.getInstance("MD5");
-        md.update(password.getBytes());
-        byte[] digest = md.digest();
-        String myHash = DatatypeConverter
-                .printHexBinary(digest).toUpperCase();
-        System.out.println("myHash:"+myHash);
+        md.update(password.getBytes());  // Different!!!
+        byte[] digest = md.digest();     // Different!!!
+        String myHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+        System.out.println("myHash:  "+myHash);
+
+        //MD5 Test3
+        String password2 = "ILoveJava";
+        MessageDigest md2 = MessageDigest.getInstance("MD5");
+
+        byte[] digest2 = md2.digest(password2.getBytes());  // Different!!!
+
+        String myHash2 = DatatypeConverter.printHexBinary(digest2).toUpperCase();
+        System.out.println("myHash2: "+myHash2);
+
+        //Make sure the difference between MD5 Test2 and MD5 Test3
+
     }
 }
 
