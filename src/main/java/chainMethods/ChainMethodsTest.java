d77edef2f051aef349e07a3ac2ead1544c645f20
spyspy <spyhopping101@gmail.com>
2024-08-01 15:38:13 +0800

    2024-08-01 update office

---
+++ src/main/java/chainMethods/ChainMethodsTest.java
@@ -0,0 +1,8 @@
+package chainMethods;
+
+public class ChainMethodsTest {
+
+    Person p = new Person().setName("尼爾森").setAddress("廢邦邦").setAge(1);
+
+}
+
---
+++ src/main/java/chainMethods/Person.java
@@ -0,0 +1,37 @@
+package chainMethods;
+
+public class Person{
+    String name;
+    String address;
+    int age;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Person setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person setAge(int age) {
+        this.age = age;
+        return this;
+    }
+}
