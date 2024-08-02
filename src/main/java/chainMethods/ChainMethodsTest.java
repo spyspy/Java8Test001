c84949ee630819943f0178132482233be2e73e8d
spyspy <spyhopping101@gmail.com>
2024-08-02 16:02:17 +0800

    updtae

--- src/main/java/chainMethods/ChainMethodsTest.java
+++ src/main/java/chainMethods/ChainMethodsTest.java
@@ -2,7 +2,10 @@ package chainMethods;
 
 public class ChainMethodsTest {
 
-    Person p = new Person().setName("尼爾森").setAddress("廢邦邦").setAge(1);
+    Person p = new Person()
+            .setName("尼爾森")
+            .setAddress("廢邦邦")
+            .setAge(1);
 
 }
 
